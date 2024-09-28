2ba54ed7a6de8b3f66e1396d06d279b1adb46996
Katie Reeves <36704503+katiereeves@users.noreply.github.com>
2024-09-28 18:29:32 -0400

    more uniform formatting

--- src/spider.h
+++ src/spider.h
@@ -3,12 +3,12 @@
 
 #define URL_MAX_LENGTH 2048 // https://www.sitemaps.org/protocol.html [This value must be less than 2048 characters.]
 
-typedef struct _list {
+typedef struct _list{
     char *data;
     struct _list *next;
 } list;
 
-void append_list(list *head, list *new) {
+void append_list(list *head, list *new){
     list *lptr = head;
     for(;lptr->next; lptr = lptr->next);
     lptr->next = new;
